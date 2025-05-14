2581bacad02e9a69f7cc89fc67fb5e6884fe856e
scipunch <scipunch@gmail.com>
2025-05-14 17:09:44 +0200

    refactor: Move bot setup into special module

--- app/bot.py
+++ app/bot.py
@@ -1,16 +1,36 @@
 import logging
 
 from telebot import TeleBot
-from telebot.types import Message
 
 log = logging.getLogger(__name__)
 
+SPECIAL_MENTION = "@all"
+
 
 def init(bot: TeleBot) -> None:
-    @bot.message_handler(commands=["start", "help"])
-    def send_welcome(message: Message) -> None:
-        """Handle /start and /help commands."""
-        bot.reply_to(
-            message,
-            "Send me a YouTube or Instagram link, and I'll download the media for you!",
-        )
+    @bot.message_handler(commands=["start"])
+    def main(message):
+        bot.send_message(message.chat.id, "hello")
+        bot.send_message(message.chat.id, message)
+
+    @bot.message_handler(
+        func=lambda message: message.chat.type in ["group", "supergroup"]
+        and SPECIAL_MENTION in message.text.lower()
+    )
+    def mention_all_admins(message):
+        chat_id = message.chat.id
+
+        admins = bot.get_chat_administrators(chat_id)
+
+        mentions = []
+        for admin in admins:
+            user = admin.user
+            mention = f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
+            mentions.append(mention)
+
+        if mentions:
+            reply_text = "Внимание: " + ", ".join(mentions)
+        else:
+            reply_text = "Не удалось получить список администраторов."
+
+        bot.send_message(chat_id, reply_text, parse_mode="HTML")
--- bot.py
+++ bot.py
@@ -3,36 +3,4 @@ import telebot  # type: ignore
 bot = telebot.TeleBot("")
 
 
-@bot.message_handler(commands=["start"])
-def main(message):
-    bot.send_message(message.chat.id, "hello")
-    bot.send_message(message.chat.id, message)
-
-
-SPECIAL_MENTION = "@all"
-
-
-@bot.message_handler(
-    func=lambda message: message.chat.type in ["group", "supergroup"]
-    and SPECIAL_MENTION in message.text.lower()
-)
-def mention_all_admins(message):
-    chat_id = message.chat.id
-
-    admins = bot.get_chat_administrators(chat_id)
-
-    mentions = []
-    for admin in admins:
-        user = admin.user
-        mention = f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
-        mentions.append(mention)
-
-    if mentions:
-        reply_text = "Внимание: " + ", ".join(mentions)
-    else:
-        reply_text = "Не удалось получить список администраторов."
-
-    bot.send_message(chat_id, reply_text, parse_mode="HTML")
-
-
 bot.polling(none_stop=True)
